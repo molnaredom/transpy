5c56b5a1f16f6c58e4c0a5540c72f2298552bac6
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2023-04-24 07:52:59 +0200

    match identálási gond + black mostmár jól működik + refaktorok

--- analyzer/transformer.py
+++ analyzer/transformer.py
@@ -7,43 +7,46 @@ from functools import lru_cache
 import difflib
 from tokenize import generate_tokens
 
+
 @lru_cache(maxsize=128)
 def is_inside_if(lines, pos, base_indent):
-    #Supposing that base_indent is the indentation of the If-node returns True if the lines[pos] is inside that If-node
+    # Supposing that base_indent is the indentation of the If-node returns True if the lines[pos] is inside that If-node
     indent = indentation(lines[pos])
-    #print(f"Indent at line: ({lines[pos].strip()}): {indent}")
+    # print(f"Indent at line: ({lines[pos].strip()}): {indent}")
     stripped = lines[pos].strip()
 
-    if (stripped.startswith("#") or not bool(stripped)): # Empty / comment lines, have to check ahead if possible.
-        if pos+1 < len(lines):
-            return is_inside_if(lines, pos+1, base_indent)
+    if (stripped.startswith("#") or not bool(stripped)):  # Empty / comment lines, have to check ahead if possible.
+        if pos + 1 < len(lines):
+            return is_inside_if(lines, pos + 1, base_indent)
         return False
 
     if indent > base_indent:
         # Line is indented inside base_indent
         return True
-    elif indent == base_indent: # Line is at the same indentation
+    elif indent == base_indent:  # Line is at the same indentation
         if (stripped.startswith("else:") or stripped.startswith("elif") or stripped.startswith(")")):
-            return True # its just another branch or multiline test
+            return True  # its just another branch or multiline test
 
     return False
 
-def count_actual_lines( lines, pos):
+
+def count_actual_lines(lines, pos):
     # At pos is the beginning of the If-node in the source code's string of lines
     offset = 0
     if not lines[pos].startswith('if'):
-        while not lines[pos+offset].strip().startswith('if'):
+        while not lines[pos + offset].strip().startswith('if'):
             offset -= 1
 
-    base_indent = indentation(lines[pos+offset])
-    #print(f"Base-Indent at line: ({lines[pos+offset].strip()}): {base_indent}")
+    base_indent = indentation(lines[pos + offset])
+    # print(f"Base-Indent at line: ({lines[pos+offset].strip()}): {base_indent}")
     res = 1 - offset
     pos += 1
     while pos < len(lines) and is_inside_if(lines, pos, base_indent):
         res += 1
         pos += 1
-    #print(f" ACTUAL LINES: {res}, OFFSET: {offset}")
+    # print(f" ACTUAL LINES: {res}, OFFSET: {offset}")
     return (res, offset)
+
 
 def indentation(s, tabsize=4):
     sx = s.expandtabs(tabsize)
@@ -70,6 +73,7 @@ def count_spaces(code):
             return count
     return count
 
+
 def does_not_have_return_continue_break_yield(code):
     """
     Returns True if the given code contains any of the following statements:
@@ -88,11 +92,12 @@ def does_not_have_return_continue_break_yield(code):
     print("@@NEM TARTALMAZ")
     return True
 
+
 class Transformer(ast.NodeTransformer):
 
     def __init__(self):
         self.analyzer = Analyzer()
-        self.results = {} # Mapping the linenos of the og If-nodes to their transformed counterpart
+        self.results = {}  # Mapping the linenos of the og If-nodes to their transformed counterpart
         self.visit_recursively = config["MAIN"].getboolean("VisitBodiesRecursively")
         self.preserve_comments = config["MAIN"].getboolean("PreserveComments")
         self.logger = OutputHandler("transformer.log") if config["OUTPUT"].getboolean("AllowTransformerLogs") else None
@@ -109,28 +114,29 @@ class Transformer(ast.NodeTransformer):
         self.visited_nodes += 1
         self.analyzer.visit(node)  # szetszedi a regi kodot ast darabokra , A node ast objektum
         if node in self.analyzer.subjects.keys():  # ha IF objektum benne van az analizalhato objektumok közt ??
-            subjectNode = self.analyzer.subjects[node] # ifben a változó neve,--> astra alakitva a változó érték
+            subjectNode = self.analyzer.subjects[node]  # ifben a változó neve,--> astra alakitva a változó érték
             _cases = []  # if ágak gyűjtőhelye --> if elif else
             res = []
             for hanyadik_barnch, branch in enumerate(self.analyzer.branches[node]):
                 if branch.flat:
                     for subBranch in branch.flat:
                         pattern = self.analyzer.patterns[subBranch]
-                        transformed_branch = ast.match_case(pattern = pattern.transform(subjectNode),
-                                                            guard = pattern.guard(subjectNode),
-                                                            body = subBranch.body)
+                        transformed_branch = ast.match_case(pattern=pattern.transform(subjectNode),
+                                                            guard=pattern.guard(subjectNode),
+                                                            body=subBranch.body)
                         try:
                             # print(ast.parse(ast.unparse(transformed_branch)))
                             _cases.append(transformed_branch)
                         except SyntaxError:
                             return None
                 else:
-                    _pattern = ast.MatchAs() if branch.test is None else self.analyzer.patterns[branch].transform(subjectNode)
+                    _pattern = ast.MatchAs() if branch.test is None else self.analyzer.patterns[branch].transform(
+                        subjectNode)
                     _guard = None if branch.test is None else self.analyzer.patterns[branch].guard(subjectNode)
                     temp = ast.Module(body=branch.body, type_ignores=[])
                     if self.visit_recursively:
                         self.generic_visit(temp)
-                    transformed_branch = ast.match_case(pattern = _pattern, guard = _guard, body = temp.body)
+                    transformed_branch = ast.match_case(pattern=_pattern, guard=_guard, body=temp.body)
                     _cases.append(transformed_branch)
 
             def utolsosor():
@@ -145,19 +151,19 @@ class Transformer(ast.NodeTransformer):
                     sor += "  " + comments[f"in{ast_sorok}"]
                 if f"out{ast_sorok + 1}" in comments and not utolsosor():
                     elozo_sor_indentalasa = count_spaces(sor.splitlines()[0])
-                    sor += "\n" + " "*4 +" " * elozo_sor_indentalasa + comments[f"out{ast_sorok + 1}"]
+                    sor += "\n" + " " * 4 + " " * elozo_sor_indentalasa + comments[f"out{ast_sorok + 1}"]
                     ast_sorok += 1
                     return sorellenor(sor)
-                return "\n".join([" "*4 + i for i in sor.splitlines()]) # minden sorba 4 space a match miatt
-                    # *4 indentalas szama , +4 mert mindig bentebb kerul 1 sorral ami ciklusobn belul van
+                return sor  # minden sorba 4 space a match miatt
+                # *4 indentalas szama , +4 mert mindig bentebb kerul 1 sorral ami ciklusobn belul van
 
-            ast_atalakitott = ast.Match(subject = subjectNode, cases=_cases) # mar atalakitott cucc
+            ast_atalakitott = ast.Match(subject=subjectNode, cases=_cases)  # mar atalakitott cucc
             unparsed_ast = ast.unparse(ast_atalakitott)
             print("ÉÉÉÉÉ\n", unparsed_ast)
             ast_sorok = node.test.lineno
             for sorszam, sor in enumerate(unparsed_ast.splitlines()):
                 ujsor = sorellenor(sor)
-                print("Ú", ujsor)
+                # print("Ú", ujsor)
                 res.append(ujsor + "\n")
                 if "match" not in sor:
                     ast_sorok += 1
@@ -166,14 +172,14 @@ class Transformer(ast.NodeTransformer):
             # print("res")
             # [print(i) for i in res]
 
-            self.results[node.test.lineno-1] = res
+            self.results[node.test.lineno - 1] = res
             result = ast.Match(subject=subjectNode, cases=_cases)
             print("result", ast.unparse(result))
             return result
         elif self.visit_recursively:
             curr_node = node
             while isinstance(curr_node, ast.If):
-                temp = ast.Module(body = curr_node.body)
+                temp = ast.Module(body=curr_node.body)
                 self.generic_visit(temp)
                 curr_node.body = temp.body
                 if len(curr_node.orelse):
@@ -181,7 +187,7 @@ class Transformer(ast.NodeTransformer):
                         curr_node = curr_node.orelse[0]
                         continue
                     else:
-                        temp = ast.Module(body = curr_node.orelse)
+                        temp = ast.Module(body=curr_node.orelse)
                         self.generic_visit(temp)
                         curr_node.orelse = temp.body
                 break
@@ -204,89 +210,81 @@ class Transformer(ast.NodeTransformer):
             self.analyzer.file = file
             global comments
             comments = None
-            if self.preserve_comments:
-                comments = {}
-                src.seek(0)
-                tokens = generate_tokens(src.readline)
-                cikluskezdet = True
-                for token in tokens:
-                    if token.type == 61:
-                        if cikluskezdet:
-                            comments[f"out{token.start[0]}"] = token.string # egeszsoros komment
-                            cikluskezdet = False
-                        else:
-                            comments[f"in{token.start[0]}"] = token.string # kodot tartalmazo sorban komment
-                            cikluskezdet = False
-                    else:
-                        if token.string == "\n":
-                            cikluskezdet = True
-                        else:
-                            cikluskezdet = False
-                print(comments)
+            self.preserved_comments(src)
+            self.visit(tree)
 
-            self.visit(tree)  # itt kezdődik az if cuccok nezese
             if len(self.results.keys()) == 0:
                 return
-
             src.seek(0)
             src_lines = tuple(src.readlines())
 
         print(self.results.keys())
-        # Writing the (transformed) file
 
         self.nemtommi(src_lines)
 
-        subprocess.Popen(f"python3.11 -m black {file}",
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-
         with open(file, "w", encoding='utf-8') as out:
+            print("FFÁÁJL", file)
             i = 0
-            # fileba iratas
-            [print("a",i, end="") for i in src_lines]
             while i < len(src_lines):
                 if i in self.results.keys():
                     indent = indentation(src_lines[i])
-                    print("INDENT", indent)
-                    # print("indent: ", indent)
                     res = self.results[i][0]
-                    # print("RES", res)
                     for newLine in res:
-                        # print("NL", indent * " " + newLine, end="")
-
-                        out.write(4 * " " + newLine)
-                        # print("....", [indent * " " + n for n in newLine.split("\n")])
-                        # out.write("\n".join([ n for n in newLine.split("\n")]))
-                    i += self.results[i][1] -1
+                        print(indent)
+                        out.write(indent * " " + newLine)
+                    i += self.results[i][1] - 1
                 else:
-                    # print("EL: ", src_lines[i], end="", file=out)
                     out.write(src_lines[i])
                 i += 1
+        subprocess.run(["python3.11", "-m", "black", file])
 
-        # Checking for SyntaxErrors in the transformed file
-        with open(file, "r", encoding='utf-8') as f:
-            new_lines = f.read()
-            f.seek(0)
-            newlines = f.readlines()
+        # print(k.stdout)
+        # print(k.stderr)
+        # # Checking for SyntaxErrors in the transformed file
+        # with open(file, "r", encoding='utf-8') as f:
+        #     new_lines = f.read()
+        #     f.seek(0)
+        #     newlines = f.readlines()
+        #
+        # # try:
+        # #     ast.parse(new_lines)
+        # # except SyntaxError as err:
+        # #     self.log(f"REVERTING {file}: SyntaxError: {err.msg} - line({err.lineno})")
+        # #     print("SYNTAX ERR", f"REVERTING {file}: SyntaxError: {err.msg} - line({err.lineno})")
+        # #     with open(file, "w", encoding='utf-8') as f:
+        # #         f.writelines(src_lines)
+        # #     return
+        #
+        # if self.generate_diffs and OutputHandler.OUTPUT_FOLDER:
+        #     import os
+        #     from pathlib import Path
+        #
+        #     diff = difflib.context_diff(src_lines, newlines, fromfile=str(file), tofile=str(file))
+        #     diffile = (OutputHandler.OUTPUT_FOLDER / 'diffs' / f'{os.path.basename(file)}-diffs.diff').resolve()
+        #
+        #     with open(diffile, 'w', encoding='utf-8') as f:
+        #         f.writelines(diff)
 
-        try:
-            ast.parse(new_lines)
-        except SyntaxError as err:
-            self.log(f"REVERTING {file}: SyntaxError: {err.msg} - line({err.lineno})")
-            print("SYNTAX ERR", f"REVERTING {file}: SyntaxError: {err.msg} - line({err.lineno})")
-            with open(file, "w", encoding='utf-8') as f:
-                f.writelines(src_lines)
-            return
-
-
-        if self.generate_diffs and OutputHandler.OUTPUT_FOLDER:
-            import os
-            from pathlib import Path
-
-            diff = difflib.context_diff(src_lines, newlines, fromfile= str(file), tofile= str(file))
-            diffile = (OutputHandler.OUTPUT_FOLDER / 'diffs' / f'{os.path.basename(file)}-diffs.diff').resolve()
-
-            with open(diffile, 'w', encoding='utf-8') as f:
-                f.writelines(diff)
+    def preserved_comments(self, src):
+        global comments
+        if self.preserve_comments:
+            comments = {}
+            src.seek(0)
+            tokens = generate_tokens(src.readline)
+            cikluskezdet = True
+            for token in tokens:
+                if token.type == 61:
+                    if cikluskezdet:
+                        comments[f"out{token.start[0]}"] = token.string  # egeszsoros komment
+                        cikluskezdet = False
+                    else:
+                        comments[f"in{token.start[0]}"] = token.string  # kodot tartalmazo sorban komment
+                        cikluskezdet = False
+                else:
+                    if token.string == "\n":
+                        cikluskezdet = True
+                    else:
+                        cikluskezdet = False
 
     def nemtommi(self, src_lines):
         i = 0
